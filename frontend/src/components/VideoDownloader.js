d0b9b8fc7edac74ea113d0a9c510526f53dbf395
EspinAndres01 <andresboki2000@gmail.com>
2024-08-09 22:42:45 -0500

    1

---
+++ frontend/src/components/VideoDownloader.js
@@ -0,0 +1,74 @@
+// src/components/VideoDownloader.js
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './VideoDownloader.css';
+
+const VideoDownloader = () => {
+    const [url, setUrl] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:5000');
+        
+        ws.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            if (message.progress !== null) {
+                setProgress(message.progress);
+            }
+        };
+
+        return () => {
+            ws.close();
+        };
+    }, []);
+
+    const handleDownload = async () => {
+        if (!url) {
+            setError('Please enter a URL');
+            return;
+        }
+
+        setError('');
+        setLoading(true);
+
+        try {
+            const response = await axios.post('http://localhost:5000/api/download', { url }, { responseType: 'blob' });
+
+            const contentType = response.headers['content-type'];
+            const blob = new Blob([response.data], { type: contentType });
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = 'video.mp4';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } catch (err) {
+            console.error('Error during download:', err);
+            setError('Error downloading the video');
+        } finally {
+            setLoading(false);
+            setProgress(0);
+        }
+    };
+
+    return (
+        <div className="video-downloader">
+            <input
+                type="text"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                placeholder="Enter YouTube video URL"
+            />
+            <button onClick={handleDownload} disabled={loading}>
+                {loading ? 'Downloading...' : 'Download Video'}
+            </button>
+            {loading && <p className="progress">Progress: {progress}%</p>}
+            {error && <p>{error}</p>}
+        </div>
+    );
+};
+
+export default VideoDownloader;
---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Registra el service worker
+serviceWorker.register();
